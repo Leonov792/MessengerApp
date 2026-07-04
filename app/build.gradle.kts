bd094113fc6361c03cecddabd4e6eaf3f3a6bad9
Leonov792 <Leonov792@users.noreply.github.com>
2026-07-04 08:51:47 +0300

    Fix dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,4 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2"); implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.room:room-runtime:2.6.1"); implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0"); implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.firebase:firebase-bom:32.7.0"); implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
 }
